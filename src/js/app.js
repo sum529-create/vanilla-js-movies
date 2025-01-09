a0d8a49159d01f95876e2087eb537ad6c50ff868
노수민 <nosumin29@gmail.com>
2025-01-09 18:05:05 +0900

    refactor: 영화 아이템 innerHtml 별도의 함수 분리

--- src/js/app.js
+++ src/js/app.js
@@ -3,40 +3,48 @@ import { loadApiKey, fetchMovieList, searchMovie } from "./api.js";
 const $movieCard = document.querySelector(".movie-list");
 const $movieSearch = document.querySelector("#movie-search");
 (() => getMovieList())();
+
+const getMovieItem = (data) => {
+  if (data) {
+    $movieCard.innerHTML = "";
+    const movieList = data
+      .map((e) => {
+        let title = e.title;
+        let grade = Math.round(e.vote_average * 100) / 100;
+        let imgUrl = `https://image.tmdb.org/t/p/w500${e.poster_path}` || "";
+
+        return `
+        <li>
+          <div class="img-area">
+            <img
+              src="${imgUrl}"
+              alt="${title}"
+            />
+            <div class="img-area-info">
+              <h2 class="card-title">${title}</h2>
+              <p class="card-grade">
+                <i class="material-symbols-outlined">
+                  star_rate
+                </i> 
+                <span>${grade}</span>
+              </p>
+            </div>
+          </div>
+        </li>
+      `;
+      })
+      .join("");
+
+    $movieCard.innerHTML = movieList;
+  }
+};
+
 async function getMovieList() {
   await loadApiKey();
   const res = await fetchMovieList();
 
   const data = res.results;
-  const movieList = data
-    .map((e) => {
-      let title = e.title;
-      let grade = Math.round(e.vote_average * 100) / 100;
-      let imgUrl = `https://image.tmdb.org/t/p/w500${e.poster_path}` || "";
-
-      return `
-      <li>
-        <div class="img-area">
-          <img
-            src="${imgUrl}"
-            alt="${title}"
-          />
-          <div class="img-area-info">
-            <h2 class="card-title">${title}</h2>
-            <p class="card-grade">
-              <i class="material-symbols-outlined">
-                star_rate
-              </i> 
-              <span>${grade}</span>
-            </p>
-          </div>
-        </div>
-      </li>
-    `;
-    })
-    .join("");
-
-  $movieCard.innerHTML = movieList;
+  getMovieItem(data);
 }
 
 const throttle = (func, delay) => {
@@ -58,34 +66,7 @@ $movieSearch.addEventListener(
       return await getMovieList();
     }
     const data = res.results;
-    $movieCard.innerHTML = "";
-    const movieList = data
-      .map((e) => {
-        let title = e.title;
-        let grade = Math.round(e.vote_average * 100) / 100;
-        let imgUrl = `https://image.tmdb.org/t/p/w500${e.poster_path}` || "";
 
-        return `
-          <li>
-            <div class="img-area">
-              <img
-                src="${imgUrl}"
-                alt="${title}"
-              />
-              <div class="img-area-info">
-                <h2 class="card-title">${title}</h2>
-                <p class="card-grade">
-                  <i class="material-symbols-outlined">
-                    star_rate
-                  </i> 
-                  <span>${grade}</span>
-                </p>
-              </div>
-            </div>
-          </li>
-        `;
-      })
-      .join("");
-    $movieCard.innerHTML = movieList;
+    getMovieItem(data);
   }, 300)
 );
